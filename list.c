b54cc75a777e0c2305c94d69c7c307f10eaebb4e
astrotycoon <astrotycoon@yahoo.cn>
2011-11-15 19:37:26 +0800

    update

--- list.c
+++ list.c
@@ -8,7 +8,7 @@ extern list_node_t* list_node_create(void* data)
 {
 	list_node_t* node_ret = NULL;
 
-	if ((node_ret =(list_node_t *)malloc(sizeof(list_node_t))) == NULL)
+	if ((node_ret =(list_node_t *)LIST_MALLOC(sizeof(list_node_t))) == NULL)
 	{
 		errno = ENOMEM;
 		return NULL;
@@ -26,7 +26,7 @@ extern list_t* list_create(void)
 {
 	list_t* list_ret = NULL;
 
-	if ((list_ret = (list_t *)malloc(sizeof(list_t))) == NULL)
+	if ((list_ret = (list_t *)LIST_MALLOC(sizeof(list_t))) == NULL)
 	{
 		errno = ENOMEM;
 		return NULL;
@@ -37,7 +37,7 @@ extern list_t* list_create(void)
 	list_ret->tail = NULL;
 	list_ret->destroy = NULL;
 	list_ret->mathc = NULL;
-	list_ret->show = NULL;
+	list_ret->duplicate = NULL;
 
 	return list_ret;
 }
@@ -67,7 +67,7 @@ extern void list_destroy(list_t* list)
 	LIST_FREE(list);
 }
 
-extern list_node_t* list_prepend(list_t* list, list_node_t* node)
+extern list_t* list_prepend(list_t* list, void* data)
 {
 	if (NULL == list || NULL == data)
 	{
@@ -75,38 +75,55 @@ extern list_node_t* list_prepend(list_t* list, list_node_t* node)
 		return NULL;
 	}
 
+	list_node_t* new_node = NULL;
+	if ((new_node = (list_node_t *)LIST_MALLOC(sizeof(list_node))) == NULL)
+	{
+		errno = ENOMEM;
+		return NULL;
+
+	}
+	new_node->data = data;
+	
 	if (list->len)
 	{
-		node->next = list->head;
+		new_node->next = list->head;
 		node->prev = NULL;
-		list->head->prev = node;
-		list->head = node;
+		list->head->prev = new_node;
+		list->head = new_node;
 	}
 	else
 	{
-		list->head = list->tail = node;
-		node->prev = node->next = NULL;	
+		list->head = list->tail = new_node;
+		new_node->prev = new_node->next = NULL;	
 	}
 	
 	++list->len;
 
-	return node;
+	return list;
 }
 	
-extern list_node_t* list_append(list_t* list, list_node_t* node)
+extern list+_t* list_append(list_t* list, void* data)
 {
 	if (NULL == list || NULL == node)
 	{
 		errno = EINVAL;
 		return NULL;
 	}
+	
+	list_node_t* new_node = NULL;
+	if ((new_node = (list_node_t *)LIST_MALLOC(sizeof(list_node))) == NULL)
+	{
+		errno = ENOMEM;
+		return NULL;
+	}
+	new_node->data = data;
 
 	if (list->len)
 	{
-		node->next = NULL;
-		node->prev = list->tail;
-		list->tail->next = node;
-		list->tail = node;
+		new_node->next = NULL;
+		new_node->prev = list->tail;
+		list->tail->next = new_node;
+		list->tail = new_node;
 	}
 	else
 	{
@@ -116,36 +133,76 @@ extern list_node_t* list_append(list_t* list, list_node_t* node)
 	
 	++list->len;
 	
-	return node;
+	return list;
 
 }
 
-/* insert data into list "list" in the order after "node", if "node" is NULL
- * the data is inserted as the head */
-extern bool list_insert_node_at_front(list_t* list, list_node_t* node, const void* data)
+/* insert data into list "list" in the order before "old_node" */
+extern bool list_insert_node_at_front(list_t* list, list_node_t* old_node, const void* data)
 {
-	if (NULL == list)
+	if (NULL == list || NULL == old_node || NULL == data)
 	{
 		errno = EINVAL;
 		return false;
 	}
 
-	list_node_t* newnode = NULL;
-	if ((newnode = (list_node_t *)malloc(list_node_t)) == NULL)
+	list_node_t* new_node = NULL;
+	if ((new_node = (list_node_t *)LIST_MALLOC(list_node_t)) == NULL)
 	{
 		errno = ENOMEM;
 		return false;
 	} 
 	newnode->data = data;
 
-	if (node)
+	new_node->next = old_node;
+	new_node->prev = old_node->prev;
+	if (list->head == old_node)
 	{
-		
+		list->head = new_node;
 	}
-	else
-	{}
+	if (new_node->prev != NULL)
+	{
+		new_node->prev->next = new_node;
+	}
 	++list->len;
-	return false;
+
+	return true;
+}
+
+/* insert data into list "list" in the order after "old_node"*/
+extern bool list_insert_node_at_after(list_t* list, list_node_t* old_node, const void* data)
+{
+	if (NULL == list || NULL == old_node || NULL == data)
+	{
+		errno = EINVAL;
+		return false;
+	}
 	
+	list_nodt_t* new_node = NULL;
+	if ((new_node = (list_node_t *)LIST_MALLOC(sizeof(list_node_t))) == NULL)
+	{
+		errno = ENOMEM;
+		return false;
+	}
+	new_node->data = data;
+
+	new_node->next = old_node->next;
+	new_node->prev = old_node;
+	if (old_node == list->tail)
+	{
+		list->tail = new_node;
+	}
+	if (new_node->next != NULL)
+	{
+		new_node->next->prev = new_node;
+	}
+
+	++list->len;
+	return true;
+}
+
+extern bool list_insert_node_at_index(list_t* list, size_t index, const void* data)
+{
+	if (NULL == list || NULL == data || index)
 
 }
